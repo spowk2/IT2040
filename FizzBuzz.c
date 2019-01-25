71f028ab4ec1e81ed4e2b8a65443cf49060c1991
Sean O'Connor <46941500+spowk2@users.noreply.github.com>
2019-01-25 02:17:29 -0600

    Add files via upload

---
+++ FizzBuzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+
+  for(int i=1;i<=100;++i){
+    if (i%15==0){
+      printf("%d FizzBuzz\n", i);
+    }
+    else if (i%3 == 0) {
+      printf("%d Fizz\n", i);
+    }
+    else if (i%5 == 0) {
+      printf("%d Buzz\n", i);
+    }
+    else {
+      printf("%d\n", i);
+    }
+
+  }
+
+}
